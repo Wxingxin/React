39dc9ce62178c999720ee6f86471b3f6cbf0964b
weijiaxing <weijiaxing765@gmail.com>
2025-05-08 22:04:57 +0800

    ts

--- zustand/demo/src/store/index.ts
+++ zustand/demo/src/store/index.ts
@@ -2,7 +2,7 @@
 import {create} from 'zustand'
 
 //使用create函数创建useStore 的 Hook
-const useStore = create(()=> {
+const useStore = create<bearsType>()(()=> {
   //return 中可以存放全局共享的 数据 和 方法
   return {
     bears: 9
