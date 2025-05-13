4eb1036dea54a4da685e6e244327c27b7c9abd71
weijiaxing <weijiaxing765@gmail.com>
2025-05-14 01:21:08 +0800

    1

--- redux/redux-demo/src/App.tsx
+++ redux/redux-demo/src/App.tsx
@@ -1,12 +1,12 @@
-import Home from "@/components/Home"
+import Home from "@/components/Home";
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1 onClick={() => {}}>App</h1>
       <Home></Home>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
