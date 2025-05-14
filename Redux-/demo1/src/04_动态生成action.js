73d47a876fc3c53811d11153b09826139eb766c4
weijiaxing <weijiaxing765@gmail.com>
2025-05-14 12:58:03 +0800

    1

---
+++ Redux-/demo1/src/04_动态生成action.js
@@ -0,0 +1,22 @@
+const store = require('./store/index')
+
+const unSubscribe = store.subscribe(()=> {
+  console.log('store is change :', store.getState())
+})
+
+const change_nameAction = (name) => {
+  return {
+    type: 'change_name',
+    name
+  }
+}
+
+const change_addressAction = (address) => {
+  return {
+    type: 'change_address',
+    address
+  }
+}
+
+store.dispatch(change_nameAction('wei'))
+store.dispatch(change_addressAction('nn'))
